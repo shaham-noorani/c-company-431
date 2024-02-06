ae327fab5f2c0101209b18f200ab5c9dc0c3d379
ameelaziz <ameelaziz@tamu.edu>
2024-02-06 17:15:32 -0600

    added more user data and updated test cases

--- c_company/spec/unit/member_event_spec.rb
+++ c_company/spec/unit/member_event_spec.rb
@@ -2,7 +2,16 @@
 require 'rails_helper'
 
 RSpec.describe MemberEvent, type: :model do
-  let(:user) { User.create(first_name: 'John', last_name: 'Doe', email: 'john@example.com', role: 'member') }
+  let(:user){
+    User.create(
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'john@example.com',
+      role: 'member',
+      class_year: 'fish', # Adjust the value based on your needs
+      military_affiliation: 'Air Force', # Adjust the value based on your needs
+      military_branch: 'USAF' # Adjust the value based on your needs
+    )}
   let(:activity_type) { ActivityType.create(name: 'Running', description: 'Outdoor physical activity') }
   let(:activity) { Activity.create(name: 'Morning Run', activity_type: activity_type, description: 'A refreshing morning jog') }
   let(:event) { Event.create(name: 'Team Building', start_time: Time.parse("09:00:00"), end_time: Time.parse("12:00:00"), activity: activity) }
