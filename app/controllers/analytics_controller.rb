681ae93e41f7a964f384d082d3a2c8ff2b4dd807
ameel <ameelaziz@gmail.com>
2024-03-02 23:42:36 -0600

    added logs

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,11 +2,17 @@ class AnalyticsController < ApplicationController
     def index
         @activity_counts_by_type = Activity.joins(:activity_type).group('activity_types.id').count
 
-        @activity_counts_by_type2 = MemberActivity.joins(activity: :activity_type)
+        @memberactivity_counts_by_type = MemberActivity.joins(activity: :activity_type)
                                           .group('activity_types.id')
                                           .count
 
+        @memberactivity_completed_by_type = MemberActivity.joins(activity: :activity_type).where(completed: true).group('activity_types.id').count
+
+        @memberactivity_counts_by_activity = MemberActivity.joins(:activity).group('activities.id').count
+
+        @memberactivity_completed = MemberActivity.joins(:activity).where(completed: true).group('activities.id').count
  
+        @member_activities = MemberActivity.includes(:activity, user: :platoon).all
     
     end
   end
