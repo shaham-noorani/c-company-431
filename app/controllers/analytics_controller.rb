461999930f10a87320eaa9791715a9653d1fb762
ameel <ameelaziz@gmail.com>
2024-03-02 21:54:09 -0600

    table

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,8 +1,13 @@
 class AnalyticsController < ApplicationController
     def index
-      @activity_counts_by_type = Activity.joins(:activity_type)
-                                         .group('activity_types.id')
-                                         .count
+        @activity_counts_by_type = Activity.joins(:activity_type).group('activity_types.id').count
+
+        @activity_counts_by_type2 = MemberActivity.joins(activity: :activity_type)
+                                          .group('activity_types.id')
+                                          .count
+
+ 
+    
     end
   end
   
