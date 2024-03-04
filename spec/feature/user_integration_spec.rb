04dfa5b631c16a6320731098d5cbb5fe51d13bdb
keeganasmith2003 <keeganasmith2003@tamu.edu>
2024-03-04 06:27:42 +0000

    simulate oauth before user integration

--- spec/feature/user_integration_spec.rb
+++ spec/feature/user_integration_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'User management', type: :feature do
+  before do
+    User.create!(first_name: "Test", last_name: "User", email: "user@example.com", role: "admin")
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+        provider: 'google_oauth2',
+        info: {
+        email: 'user@example.com',
+        first_name: 'Test',
+        last_name: 'User'
+        }
+    })
+  end
   describe 'destroying a user' do
     let!(:user) { User.create(first_name: 'joe', last_name: "bob", role: "pleb", email: "joe@gmail.com") }
     
     it 'destroys the user' do
-      User.create(first_name: "joe", last_name: "bob", role: "admin", email: "joe1@gmail.com")
-      session[":useremail"] = "joe1@gmail.com"
+      
+      visit '/auth/google_oauth2'
       visit user_path(user)
 
       click_on 'Destroy this user'
