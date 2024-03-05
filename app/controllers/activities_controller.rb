c806bf0de8379b71fed84c97c0ad43a691ed7384
keeganasmith2003 <keeganasmith2003@tamu.edu>
2024-03-05 05:05:20 +0000

    fix with rubocop

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -19,205 +19,169 @@ class ActivitiesController < ApplicationController
      # GET /activities/1/edit
      def edit; end
 
-  # POST /activities or /activities.json
-  def create
-    @activity = Activity.new(activity_params)
+     # POST /activities or /activities.json
+     def create
+          @activity = Activity.new(activity_params)
 
-    respond_to do |format|
-      if @activity.save
-        format.html { redirect_to @activity, notice: 'Activity was successfully created.' }
-        format.json { render :show, status: :created, location: @activity }
+          respond_to do |format|
+               if @activity.save
+                    format.html { redirect_to(@activity, notice: 'Activity was successfully created.') }
+                    format.json { render(:show, status: :created, location: @activity) }
 
-      else
-        format.html { render(:new, status: :unprocessable_entity) }
-        format.json { render(json: @activity.errors, status: :unprocessable_entity) }
-      end 
-    end
-  end
+               else
+                    format.html { render(:new, status: :unprocessable_entity) }
+                    format.json { render(json: @activity.errors, status: :unprocessable_entity) }
+               end
+          end
+     end
 
+     def reassign
+          @activity = Activity.find(params[:id])
+          @platoons = Platoon.all
+          @user = User.all
+     end
 
-  def reassign
-    @activity = Activity.find(params[:id])
-    @platoons = Platoon.all
-    @user = User.all
+     # POST /activities/1/assign_to_platoon
+     def assign_to_platoon
+          platoon = Platoon.find(params[:activity][:platoon_id])
+          users_in_platoon = platoon.users
 
-  end
+          users_in_platoon.each do |user|
+               member_activites = MemberActivity.new(
+                    user_id: user.id,
+                    activity_id: @activity.id,
+                    date: nil,
+                    start_time: nil,
+                    end_time: nil
+               )
 
+               redirect_to(@activity, notice: 'Activity was successfully assigned to the platoon.') if member_activity.save
+          end
+     end
 
-  # POST /activities/1/assign_to_platoon
-  def assign_to_platoon
-    platoon = Platoon.find(params[:activity][:platoon_id])
-    users_in_platoon = platoon.users
-      
-    users_in_platoon.each do |user|
-      member_activites = MemberActivity.new(
-        user_id: user.id,
-        activity_id: @activity.id,
-        date: nil,
-        start_time: nil,
-        end_time: nil
-      )
+     # POST /activities/1/assign_to_user
+     def assign_to_member
+          member_activity = MemberActivity.new(
+               user_id: user.id,
+               activity_id: @activity.id,
+               date: nil,
+               start_time: nil,
+               end_time: nil
+          )
+          redirect_to(@activity, notice: 'Activity was successfully assigned to the member.') if member_activity.save
+     end
 
-      if member_activity.save
-        redirect_to @activity, notice: 'Activity was successfully assigned to the platoon.'
-      end
-    end
-  end
+     def another_function
+          user_id = params[:user_id]
+          logger.info(user_id)
+          logger.info(params[:activity_id])
+          logger.info('!!!!!!!!!!!!!!!!!!!')
 
+          @activity = Activity.find(params[:activity_id])
 
+          member_activity = MemberActivity.new(
+               user_id: user_id,
+               activity_id: @activity.id,
+               date: nil,
+               start_time: nil,
+               end_time: nil
+          )
 
-  # POST /activities/1/assign_to_user
-  def assign_to_member
+          redirect_to(@activity, notice: 'Activity was successfully assigned to the member.') if member_activity.save
+     end
 
+     def assign_member
+          @user_id = params[:user_id]
+          @users = User.all
+          @activity = Activity.find(params[:activity_id])
+          logger.info('works!!!!!!!!!!!!!!!!!!!!!')
+          logger.info(params[:user_id])
 
+          # Ensure params[:activity] is present
+          if params[:user_id].present?
+               user = User.find(params[:user_id])
 
-    member_activity = MemberActivity.new(
-      user_id: user.id,
-      activity_id: @activity.id,
-      date: nil,
-      start_time: nil,
-      end_time: nil
-    )
-    if member_activity.save
-      redirect_to @activity, notice: 'Activity was successfully assigned to the member.'
-    end
+               member_activity = MemberActivity.new(
+                    user_id: user.id,
+                    activity_id: @activity.id,
+                    date: nil,
+                    start_time: nil,
+                    end_time: nil
+               )
 
-  end
+               redirect_to(@activity, notice: 'Activity was successfully assigned to the member.') if member_activity.save
+          end
+     end
 
+     def assign_platoon
+          @activity = Activity.find(params[:activity_id])
+          logger.info('platoon assign function accessed')
+          logger.info(params[:platoon_id])
 
-  def another_function
-    user_id = params[:user_id]
-    logger.info(user_id)
-    logger.info(params[:activity_id])
-    logger.info("!!!!!!!!!!!!!!!!!!!")
+          if params[:platoon_id].present?
+               platoon = Platoon.find(params[:platoon_id])
+               users_in_platoon = platoon.users
 
-    @activity = Activity.find(params[:activity_id])
+               users_in_platoon.each do |user|
+                    member_activites = MemberActivity.new(
+                         user_id: user.id,
+                         activity_id: @activity.id,
+                         date: nil,
+                         start_time: nil,
+                         end_time: nil
+                    )
 
-    member_activity = MemberActivity.new(
-      user_id: user_id,
-      activity_id: @activity.id,
-      date: nil,
-      start_time: nil,
-      end_time: nil
-    )
+                    redirect_to(@activity, notice: 'Activity was successfully assigned to the platoon.') if member_activity.save
+               end
+          end
+     end
 
-    if member_activity.save
-      redirect_to @activity, notice: 'Activity was successfully assigned to the member.'
-    end
-  end
+     ## Creating and assigning in one action
 
+     # def create
+     #   @activity = Activity.new(activity_params)
+     #   if @activity.save
+     #     if params[:activity][:platoon_id] != ''
+     #       logger.info "got to platoon"
+     #       platoon = Platoon.find(params[:activity][:platoon_id])
+     #       users_in_platoon = platoon.users
 
+     #       users_in_platoon.each do |user|
+     #         member_activites = MemberActivity.new(
+     #           user_id: user.id,
+     #           activity_id: @activity.id,
+     #           date: nil,
+     #           start_time: nil,
+     #           end_time: nil
+     #         )
+     #         member_activities.save
+     #       end
 
-  def assign_member
-    @user_id = params[:user_id]
-    @users = User.all
-    @activity = Activity.find(params[:activity_id])
-    logger.info("works!!!!!!!!!!!!!!!!!!!!!")
-    logger.info(params[:user_id])
-  
-    # Ensure params[:activity] is present
-    if params[:user_id].present?
-      user = User.find(params[:user_id])
-      
-      member_activity = MemberActivity.new(
-        user_id: user.id,
-        activity_id: @activity.id,
-        date: nil,
-        start_time: nil,
-        end_time: nil
-      )
-  
-      if member_activity.save
-        redirect_to @activity, notice: 'Activity was successfully assigned to the member.'
-      end
-    end
-  end
-  
+     #     elsif params[:activity][:user_id] != ''
+     #       user = User.find(params[:activity][:user_id])
+     #       logger.info "activity id: #{@activity.id}"
+     #       member_activity = MemberActivity.new(
+     #         user_id: user.id,
+     #         activity_id: @activity.id,
+     #         date: nil,
+     #         start_time: nil,
+     #         end_time: nil
+     #       )
+     #       logger.info("got to member activity save")
+     #       member_activity.save
 
+     #     end
 
-  def assign_platoon
+     #     respond_to do |format|
+     #       format.html { redirect_to(activity_url(@activity), notice: 'Activity was successfully created.') }
+     #       format.json { render(:show, status: :created, location: @activity) }
+     #     end
 
-    @activity = Activity.find(params[:activity_id])
-    logger.info("platoon assign function accessed")
-    logger.info(params[:platoon_id])
-
-    if params[:platoon_id].present?
-      platoon = Platoon.find(params[:platoon_id])
-      users_in_platoon = platoon.users
-        
-      users_in_platoon.each do |user|
-        member_activites = MemberActivity.new(
-          user_id: user.id,
-          activity_id: @activity.id,
-          date: nil,
-          start_time: nil,
-          end_time: nil
-        )
-
-        if member_activity.save
-          redirect_to @activity, notice: 'Activity was successfully assigned to the platoon.'
-        end
-      end
-    end
-
-  end 
-
-
-
-
-
-
-
-  ## Creating and assigning in one action
-
-  # def create
-  #   @activity = Activity.new(activity_params)
-  #   if @activity.save
-  #     if params[:activity][:platoon_id] != ''
-  #       logger.info "got to platoon"
-  #       platoon = Platoon.find(params[:activity][:platoon_id])
-  #       users_in_platoon = platoon.users
-      
-  #       users_in_platoon.each do |user|
-  #         member_activites = MemberActivity.new(
-  #           user_id: user.id,
-  #           activity_id: @activity.id,
-  #           date: nil,
-  #           start_time: nil,
-  #           end_time: nil
-  #         )
-  #         member_activities.save
-  #       end
-
-  #     elsif params[:activity][:user_id] != ''
-  #       user = User.find(params[:activity][:user_id])
-  #       logger.info "activity id: #{@activity.id}"
-  #       member_activity = MemberActivity.new(
-  #         user_id: user.id,
-  #         activity_id: @activity.id,
-  #         date: nil,
-  #         start_time: nil,
-  #         end_time: nil
-  #       )
-  #       logger.info("got to member activity save")
-  #       member_activity.save
-
-  #     end
-
-  #     respond_to do |format|
-  #       format.html { redirect_to(activity_url(@activity), notice: 'Activity was successfully created.') }
-  #       format.json { render(:show, status: :created, location: @activity) }
-  #     end
-    
-    
-  #   else
-  #     format.html { render(:new, status: :unprocessable_entity) }
-  #     format.json { render(json: @activity.errors, status: :unprocessable_entity) }
-  #   end
-  # end
-
-
-
+     #   else
+     #     format.html { render(:new, status: :unprocessable_entity) }
+     #     format.json { render(json: @activity.errors, status: :unprocessable_entity) }
+     #   end
+     # end
 
      # PATCH/PUT /activities/1 or /activities/1.json
      def update
--- app/controllers/platoons_controller.rb
+++ app/controllers/platoons_controller.rb
@@ -57,13 +57,13 @@ class PlatoonsController < ApplicationController
           end
      end
 
-  def my_platoon
-    @current_user = User.find_by(email: session[":useremail"])
-    @platoon = @current_user.platoon
-    @members = @platoon.users
-  end
+     def my_platoon
+          @current_user = User.find_by(email: session[':useremail'])
+          @platoon = @current_user.platoon
+          @members = @platoon.users
+     end
 
-  private
+     private
 
      # Use callbacks to share common setup or constraints between actions.
      def set_platoon
