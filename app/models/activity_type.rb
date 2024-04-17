01ff94ee229aa413305b599473df316cd763edcb
keeganasmith2003 <keeganasmith2003@tamu.edu>
2024-04-17 03:31:03 +0000

    activity type model updated to delete activities when deleted

--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -2,6 +2,8 @@
 
 # app/models/activity_type.rb
 class ActivityType < ApplicationRecord
+     has_many :activity, dependent: :destroy
+
      validates :name, presence: true, length: { maximum: 255 }
      validates :description, presence: true
 end
